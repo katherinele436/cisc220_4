a85acae0816bde43c05add78ed7c7a258de51e8f
Katherine Le <14ttul@queensu.ca>
2016-11-24 09:29:48 -0500

    New version

--- capitalizer.c
+++ capitalizer.c
@@ -7,30 +7,30 @@
 
 
 
-int main(int argc, char **argv)
-
-{
-
-	FILE *fp = fopen(argv[1],"r+");
-
-	char buff[10000], newBuff[10000];
-
-	while (1) {
-
-		fgets(buff, 10000, fp);
-
-		if ( feof(fp) ) {
-
-			break;
-
+int main(int argc, char *argv[]) {
+FILE *fp;
+char string[255];
+fp = fopen(argv[1], "r+");
+long pos = ftell(fp);
+while(fgets(string, 255, fp) != NULL) {
+	int i;
+	int len = strlen(string);
+	int wordNum = 1;
+	string[0] = toupper(string[0]);
+	for (i=1 ; i < len ; i++) {
+		if (isalpha(string[i]) && string[i-1] == ' '){
+			// only first letters of a word
+			if ((wordNum % 2) == 0) {
+			string[i] = toupper(string[i]);
+			}
+			wordNum++;
 		}
-
-		printf("%s",buff);
-
 	}
-
-	fclose(fp);
-
-	return 0;
-
+	fseek(fp, pos, SEEK_SET);
+	fprintf(fp, "%s", string);
+	fflush(fp);
+	pos = ftell(fp);
+}
+fclose(fp);
+return 0;
 }
