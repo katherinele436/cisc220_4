dc1edf3350c3c058d05fd018efbfe9110782ac80
Katherine Le <14ttul@queensu.ca>
2016-11-24 22:13:45 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+#Anna Teng 10061146
+#Henry Wang 10199252
+#Katherine Le 10188031
+#Marshall Ruse 10066247
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,8 +10,8 @@
 
   int from = 3;
   int n = 10;
-	char *a = "A string ";
-	char *b = "and a second string";
+  char *a = "A string ";
+  char *b = "and a second string";
   char *source ="A string with many characters"
   char *c;
   char *c = safeststrcat(a,b);
